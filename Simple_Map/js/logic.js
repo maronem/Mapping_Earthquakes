82fc555edb7a071cab19819893f570507277e111
maronem <maronemgm@gmail.com>
2022-09-24 14:27:12 -0400

    LA circle marker

--- Simple_Map/js/logic.js
+++ Simple_Map/js/logic.js
@@ -5,7 +5,7 @@ console.log("working");
 
 // Create the map object with a center and zoom level
     //"mapid" references the id tag in <div> of html file
-let map = L.map("mapid").setView([40.7, -94.5], 4);
+let map = L.map("mapid").setView([34.0522, -118.2437], 14);
 
 // ALT METHOD - MULTIPLE TILE LAYERS
 // Create the map object with a center and zoom level.
@@ -16,11 +16,22 @@ let map = L.map("mapid").setView([40.7, -94.5], 4);
     // zoom: 4
   // });
 
+  // Add marker to map for LA, California
+  var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+  // Add a circle marker to map for LA, California
+  L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    fillColor: "yellow",
+    color: "black",
+    fillOpacity: .1
+  }).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'styles/mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
